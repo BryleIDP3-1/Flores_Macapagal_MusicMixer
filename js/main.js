f0c92e219cdb96123bb618d3229dd7b975d9fef5
BerMacJrs <145165950+BerMacJrs@users.noreply.github.com>
2024-03-23 21:38:56 -0400

    Import Initial Test Code
    Imported Initial Test Code for JS, HTML, and CSS.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,74 @@
 console.log("JS file connected");
+
+// const btn = document.querySelectorAll('.btn');
+// const fruitsBox = document.querySelector(".fruit-box");
+
+// btn.forEach(button => {
+//     button.addEventListener('click', function(e) {
+//         button.classList.toggle('button-clicked');
+//         button.querySelector('i').classList.toggle('icon-clicked');
+//     });
+// });
+
+
+// let draggedTool = null; // Define a variable to store the dragged tool
+
+// let dropSquare = document.querySelectorAll('.drop-square');
+// let fruitIcons = document.querySelectorAll('.fruits img');
+
+// function dragStart() {
+//     console.log('started dragging this piece:', this);
+//     draggedTool = this; // Store the dragged tool
+//     setTimeout(() => {
+//         this.classList.add('hide');
+//     }, 0);
+// }
+
+// function dragOver(e) {
+//     e.preventDefault();
+//     console.log('dragged over me');
+//     this.classList.remove('hide');
+// }
+
+// function drop(e) {
+//     e.preventDefault();
+//     console.log('dropped something on me');
+//     const initialParent = draggedTool.parentNode;
+
+//     if (this.childElementCount === 0) {
+//         this.appendChild(draggedTool);
+//         playAudio(draggedTool.id); // Play audio when dropped onto a drop square
+//     } else {
+//         console.log('Oops! There is already one musical tool!');
+//         initialParent.appendChild(draggedTool);
+//     }
+//     draggedTool.classList.remove('hide');
+//     //styles for animation svg = disabled on default
+//     //when element is dropped in drop zone = css animation starts
+// }
+
+// function playAudio(selectedInstrument) {
+//     console.log(selectedInstrument);
+//     let instrument = document.createElement("audio");
+//     instrument.src = `audio/${selectedInstrument}.mp3`;
+//     instrument.load();
+//     instrument.loop = true;
+//     instrument.play();
+// }
+
+// fruitIcons.forEach(tool => tool.addEventListener("dragstart", dragStart));
+
+// dropSquare.forEach(zone => {
+//     zone.addEventListener("dragover", dragOver);
+//     zone.addEventListener("drop", drop);
+// });
+
+// function resetFruits() {
+//     fruitIcons.forEach(tool => {
+//         const initialParent = tool.dataset.initialParent; // retrieve the initial parent of the fruit icon
+//         document.querySelector(initialParent).appendChild(tool); // append the fruit icon back to its initial parent
+//     });
+// }
+
+// const resetGame = document.getElementById("resetBut");
+// resetGame.addEventListener("click", resetFruits);
