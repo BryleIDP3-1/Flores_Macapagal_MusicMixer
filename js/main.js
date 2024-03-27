f31b2d81ada134cc3c09db54f06359700edff2bb
Brylliance <brylefanshawe@gmail.com>
2024-03-27 10:22:35 -0400

    Update JS - Animate SVG
    In this commit, the JS code was updated to animated the bunny SVG when all fruits (4) are dragged in drop zones.

--- js/main.js
+++ js/main.js
@@ -5,10 +5,22 @@ document.addEventListener('DOMContentLoaded', function() {
     const btns = document.querySelectorAll('.btn');
     const dropSquares = document.querySelectorAll('.drop-square');
     const fruitIcons = document.querySelectorAll('.fruits img');
+    const bunnyDJIcon = document.getElementById('bunnyDJ');
 
     // global variables
     let draggedFruit = null;
     const selectedFruits = new Set(); 
+
+    // function to check if all icons are in drop zone
+    function allIconsInDropZone() {
+        let allInDropZone = true;
+        fruitIcons.forEach(icon => {
+            if (!icon.parentNode.classList.contains('drop-square')) {
+                allInDropZone = false;
+            }
+        });
+        return allInDropZone;
+    }
 
     // button event listeners
     btns.forEach(button => {
@@ -29,20 +41,33 @@ document.addEventListener('DOMContentLoaded', function() {
         this.classList.remove('hide');
     }
 
-    function drop(e) {
-        e.preventDefault();
-        console.log('dropped something on me');
-        const initialParent = draggedFruit.parentNode;
+// global variable to keep track of dragged items count
+let draggedItemsCount = 0;
 
-        if (this.childElementCount === 0) {
-            this.appendChild(draggedFruit);
-            playAudio(draggedFruit.id, this);
-        } else {
-            console.log('Oops! There is already one fruit!');
-            initialParent.appendChild(draggedFruit);
+// drop function
+function drop(e) {
+    e.preventDefault();
+    console.log('dropped something on me');
+    const initialParent = draggedFruit.parentNode;
+
+    if (this.childElementCount === 0) {
+        this.appendChild(draggedFruit);
+        playAudio(draggedFruit.id, this);
+
+        // Increment dragged items count
+        draggedItemsCount++;
+
+        if (draggedItemsCount === 4) {
+            // Change the src of <img id="bunnyDJ">
+            const bunnyDJImg = document.getElementById('bunnyDJ');
+            bunnyDJImg.setAttribute('src', `images/Bunny_Icon-animated.svg`);
         }
-        draggedFruit.classList.remove('hide');
+    } else {
+        console.log('Oops! There is already one fruit!');
+        initialParent.appendChild(draggedFruit);
     }
+    draggedFruit.classList.remove('hide');
+}
 
     // audio related functions
     function playAudio(selectedFruit, selectedDropzone) {
